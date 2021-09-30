09b04826eaaa227406b82075622349d3bfba24b7
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-30 13:03:56 +0530

    Added: Calculated the Lenght of Line

--- src/main/java/com/bdlz/linecomparison/LineComparison.java
+++ src/main/java/com/bdlz/linecomparison/LineComparison.java
@@ -1,7 +1,20 @@
 package com.bdlz.linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to the LineComparison Program");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter x1 co-ordinate of line: ");
+        int x1 = scanner.nextInt();
+        System.out.println("Enter x2 co-ordinate of line: ");
+        int x2 = scanner.nextInt();
+        System.out.println("Enter y1 co-ordinate of line: ");
+        int y1 = scanner.nextInt();
+        System.out.println("Enter y2 co-ordinate of line: ");
+        int y2 = scanner.nextInt();
+        scanner.close();
+        double length_Of_Line = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        System.out.println("Length of Line: " + length_Of_Line);
     }
 }
