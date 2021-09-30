7a600235f69d262f4a3204faa7921d6ba8376e39
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-30 13:45:24 +0530

    Modified: Resloved Mistakes

--- src/main/java/com/bdlz/linecomparison/LineComparison.java
+++ src/main/java/com/bdlz/linecomparison/LineComparison.java
@@ -1,6 +1,6 @@
 package com.bdlz.linecomparison;
 
-import java.util.Objects;
+import java.util.*;
 import java.util.Scanner;
 
 public class LineComparison {
@@ -25,17 +25,10 @@ public class LineComparison {
         int q2 = scanner.nextInt();
         scanner.close();
 
-        double length_Of_Line1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-        double length_Of_Line2 = Math.sqrt((p2-p1)*(p2-p1)+(q2-q1)*(q2-q1));
+        double lengthOfLine1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        double lengthOfLine2 = Math.sqrt((p2-p1)*(p2-p1)+(q2-q1)*(q2-q1));
 
-        System.out.println("Length of Line1: " + length_Of_Line1);
-        System.out.println("Length of Line2: " + length_Of_Line2);
-
-        boolean result = Objects.equals(length_Of_Line1,length_Of_Line2);
-        if (result) {
-            System.out.println("Both lines are Equal");
-        } else {
-            System.out.println("Both lines are Not Equal");
-        }
+        System.out.println("Length of Line1: " + lengthOfLine1);
+        System.out.println("Length of Line2: " + lengthOfLine2);
     }
 }
